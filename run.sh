460df44b9597d9f0b75e3476a1e02ab124409a3c
Mathis Gerdes <MathisGerdes@gmail.com>
2017-06-18 00:06:12 +0200

    Add multi-file uploads to scripts.
    Support flashing the MicroPython runtime and uploading multiple files
    to the microbit (via microfs) in the convenience-script.
    If one is called 'main.py' it is executed after resetting the microbit.
    
    Add an example for multiple files in demos.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #######################################################
 # Script for automatically mounting microbit to local #
@@ -8,30 +8,85 @@
 # needs root privelages for mounting.                 #
 #######################################################
 
-# requires uflash:
-# pip install uflash
+# requires uflash, microfs:
+# pip install uflash microfs
 
-# create local directory for mounting if it doesn't exist
-if [ -d .MICROBIT-mnt ]; then
-    echo "Mount directory exists."
+
+if [ "$1" = "-h" ]; then
+  printf "Use this script to automatically mount and push to the microbit. "
+  printf "There are four modes:\n"
+  printf "\tWith the option -h, this text is displayed.\n"
+  printf "\tIf no argument is provided, the MicroPython runtime is flashed.\n"
+  printf "\tOne python script provided, this is flashed onto the microbit.\n"
+  printf "\tGiven \"--put\" and a number of python files provided, the runtime "
+  printf "will be flashed and the python files transfered to the filesystem.\n"
+
 else
-    echo "Creating mount directory"
-    mkdir .MICROBIT-mnt
+
+  # need root privelages for mounting
+  [ $(id -u) != "0" ] && exec sudo "$0" "$@"
+
+  # local mount folder
+  MOUNT_DIR="/media/mathis/MICROBIT"
+
+  # create local directory for mounting if it doesn't exist
+  if [ -d $MOUNT_DIR ]; then
+    printf "Mount directory exists.\n"
+  else
+    printf "Creating mount directory...\n"
+    mkdir $MOUNT_DIR
+  fi
+
+  # mount MICROBIT to local directory
+  if mount | grep "$MOUNT_DIR" > /dev/null; then
+    printf "Already mounted.\n"
+  else
+    printf "Mounting microbit...\n"
+    mount /dev/disk/by-label/MICROBIT $MOUNT_DIR
+  fi
+
+
+  case $# in
+    0)
+      printf "Flashing only the MicroPython runtime."
+      # if no argument is passed, flash the MicroPython runtime
+      uflash
+      ;;
+    1)
+      # flash python file
+      uflash "$1" $MOUNT_DIR
+      ;;
+    *)
+      if [ "$1" = "--put" ]; then
+        # flash the runtime
+        uflash
+
+        echo "Waiting for compilation..."
+        umount $MOUNT_DIR
+
+        printf "Uploading files:\n"
+        shift 1
+        for py in "$@"
+        do
+          case "$py" in
+            *.py)
+              echo "uploading $py..."
+              ufs put "$py"
+              ;;
+            *)
+              echo "skipping $py."
+          esac
+        done
+      else
+        printf "Unknown argument \"$0\".\n"
+        printf "Use flag -h for help. \n"
+        exit 1
+      fi
+  esac
+
+  if mount | grep "$MOUNT_DIR" > /dev/null; then
+    # unmount MICROBIT and delete mount folder
+    umount $MOUNT_DIR
+    rmdir $MOUNT_DIR
+  fi
 fi
-
-# mount MICROBIT to local directory
-if mount | grep ".MICROBIT-mnt" > /dev/null; then
-  echo "Already mounted."
-else
-  echo "Mounting microbit..."
-  mount /dev/disk/by-label/MICROBIT ./.MICROBIT-mnt
-fi
-
-# first command line argument is py file
-FILE=$1
-
-# flash python file
-uflash $FILE ./.MICROBIT-mnt
-
-# unmount MICROBIT
-umount ./.MICROBIT-mnt
